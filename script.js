64841dc272c23fc6cd120c8f07eee6ee41c2faeb
George K <gkarv@hotmail.gr>
2022-03-03 14:04:04 +0000

    Changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function compute(){   
-    var principal = document.getElementById("principal").value;
-    if(principal<=0){
+    var principal = document.getElementById("principal");
+    if(principal.value<=0){
         alert("Please enter positive value");
         principal.focus();
         return false;
@@ -8,11 +8,11 @@ function compute(){
     else{
         var rate =  document.getElementById("rate").value;
         var years = document.getElementById("years").value;
-        var interest = principal*rate*years/100;
+        var interest = principal.value*rate*years/100;
         var  year = new Date().getFullYear() + parseInt(years);
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-        return true;
+        document.getElementById("result").innerHTML="If you deposit "+principal.value+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
     }
+    return true;
 }    
 function updateRate(){
     var rateval = document.getElementById("rate").value;
