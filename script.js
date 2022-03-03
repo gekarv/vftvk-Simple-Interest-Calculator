df6b8372c99fab7890bc05898c91e02f70891e63
George K <gkarv@hotmail.gr>
2022-03-03 13:23:18 +0000

    Final Project

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function compute(){   
+    var principal = document.getElementById("principal").value;
+    if(principal<=0){
+        alert("Please enter positive value");
+        principal.focus();
+        return false;
+    }
+    else{
+        var rate =  document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal*rate*years/100;
+        var  year = new Date().getFullYear() + parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        return true;
+    }
+}    
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}  
